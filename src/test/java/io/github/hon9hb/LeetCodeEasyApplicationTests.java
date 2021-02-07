9d71ab4f8b9deeb7071403630957f17c7ed37543
HYEBIN HONG <hon9hb@gmail.com>
2021-02-07 21:25:26 +0900

    initial commit

---
+++ src/test/java/io/github/hon9hb/LeetCodeEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hon9hb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
