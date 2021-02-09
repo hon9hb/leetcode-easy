4224e0c649120ddf3fff638dcfaf07f243deb8e8
HYEBIN HONG <hon9hb@gmail.com>
2021-02-09 22:14:12 +0900

    9.Palindrome Number

--- src/test/java/io/github/hon9hb/PalindromeNumberTest.java
+++ src/test/java/io/github/hon9hb/PalindromeNumberTest.java
@@ -7,9 +7,9 @@ public class PalindromeNumberTest {
 	void palindromeNumberTest() {
 		int x = 1349431;
 
-		// while문 방식
 		PalindromeNumber palindromeNumber = new PalindromeNumber();
 		boolean result = palindromeNumber.solution(x);
+		
 		System.out.println("Is " + x + " a palindrome? : " + result);
 	}
 }
