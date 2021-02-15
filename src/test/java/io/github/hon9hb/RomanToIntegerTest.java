9fdba833f5e0cd4af4da641f71f15b91c9397c1e
HYEBIN HONG <hon9hb@gmail.com>
2021-02-15 20:05:04 +0900

    13.Roman to Integer

--- src/test/java/io/github/hon9hb/RomanToIntegerTest.java
+++ src/test/java/io/github/hon9hb/RomanToIntegerTest.java
@@ -9,12 +9,12 @@ public class RomanToIntegerTest {
 		String s1 = "LXXIV";
 		String s2 = "MCMXCIV";
 		
-		// Map을 이용한 방식
+		// Switch문을 이용한 방식
 		RomanToInteger1 romanToInteger1 = new RomanToInteger1();
 		int result1 = romanToInteger1.solution(s1);
 		System.out.println(s1 + " refers to " + result1);
 
-		// Switch문을 이용한 방식
+		// Map을 이용한 방식
 		RomanToInteger2 romanToInteger2 = new RomanToInteger2();
 		int result2 = romanToInteger2.solution(s2);
 		System.out.println(s2 + " refers to " + result2);
